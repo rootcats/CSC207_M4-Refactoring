0876314cb89da4b98e1926f454b3c35bc30d012f
Js <rootpie09@gmail.com>
2025-11-24 04:16:26 -0500

    Task 1.1 Completed

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,33 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Returns the id of the play.
+     *
+     * @return play id
+     */
+    public String getPlayId() {
+        return playID;
+    }
+
+    /**
+     * Returns the size of audience.
+     *
+     * @return the size of audience
+     */
+    public int getAudience() {
+        return audience;
     }
 }
